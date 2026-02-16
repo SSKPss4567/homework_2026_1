223f59b86d869e2f8bbf0118dc7e0c88a4c7a0a0
SSKPss4567 <markonext@yandex.ru>
2026-02-16 22:23:06 +0300

    Исправлены замечания

--- source/transform.js
+++ source/transform.js
@@ -1,6 +1,8 @@
+'use strict';
+
 /**
  * Функция, преобразующая поля объекта с помощью переданной функции.
- * @param {Object} obj - исходный объект или массив
+ * @param {Object|Array} obj - исходный объект или массив
  * @param {Function} transformFn - функция преобразования значений
  *
  * @example
@@ -11,12 +13,18 @@
  * // returns { a: [2, 3], b: 4 }
  * transform({ a: [1, 2], b: 3 }, x => x + 1);
  *
- * @returns {Object}
+ * @returns {Object|Array}
  */
 const transform = (obj, transformFn) => {
-    if (Array.isArray(obj)) {
-        return obj.map(item => (item && typeof item === 'object') ? transform(item, transformFn) : transformFn(item));
+
+    if ((typeof obj !== 'object' && !Array.isArray(obj)) || obj === null || typeof transformFn !== 'function') {
+        return console.error("Входные данные должны быть объектом или массивом, а transformFn должен быть функцией");
     }
+
+    if (Array.isArray(obj)) {
+        return obj.map(item => (item !== null && typeof item === 'object') ? transform(item, transformFn) : transformFn(item));
+    }
+
     return Object.fromEntries(
         Object.entries(obj).map(([key, value]) => {
             if (typeof value === 'object' && value !== null) {                 
